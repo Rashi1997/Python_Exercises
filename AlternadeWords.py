c189ff4b90bde88d518518462f70b178009ea408
Rashi1997 <rdhar1997@gmail.com>
2018-10-25 16:50:09 +0530

    Update AlternadeWords.py

--- AlternadeWords.py
+++ AlternadeWords.py
@@ -1,4 +1,4 @@
-f = open("C:/Users/rasdhar/Desktop/Python Training/46/unixdict.txt")
+f = open("http://www.puzzlers.org/pub/wordlists/unixdict.txt")
 words=f.read().split("\n")
 for word in words:
     if(len(word)>5 and word[::2] in words and word[1::2] in words):
