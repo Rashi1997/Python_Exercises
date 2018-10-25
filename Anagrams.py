d561ebd30a0b522db006968dc2f050b36a2e2c70
Rashi1997 <rdhar1997@gmail.com>
2018-10-25 17:17:27 +0530

    Add files via upload

---
+++ Anagrams.py
@@ -0,0 +1,11 @@
+import itertools
+from collections import defaultdict
+f=open("C:/Users/rasdhar/Desktop/Python Training/43/unixdict.txt")
+words=f.read().split("\n")
+store=defaultdict(list)
+def check_anagrams(a,b):
+    return sorted(a)==sorted(b)
+for a , b in itertools.combinations(words,2):
+    if(check_anagrams(a,b)):
+        store[a].append(b)
+        
