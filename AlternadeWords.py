97b40af2a6f7f50d6170162eb60e4844f2719b99
Rashi1997 <rdhar1997@gmail.com>
2018-10-29 14:07:21 +0530

    Update and rename AlternadeWords.py to 46.AlternadeWords.py

--- AlternadeWords.py
+++
@@ -1,5 +0,0 @@
-f = open("http://www.puzzlers.org/pub/wordlists/unixdict.txt")
-words=f.read().split("\n")
-for word in words:
-    if(len(word)>5 and word[::2] in words and word[1::2] in words):
-        print(word+" makes "+ word[::2]+" and "+word[1::2])
