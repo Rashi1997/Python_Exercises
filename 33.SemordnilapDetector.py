108f9905d8581a8eda166e2382ffd490f3f6744b
Rashi1997 <rdhar1997@gmail.com>
2018-11-02 15:42:57 +0530

    Update 33.SemordnilapDetector.py

--- 33.SemordnilapDetector.py
+++ 33.SemordnilapDetector.py
@@ -1,4 +1,7 @@
-"""According to Wikipedia, a semordnilap is a word or phrase that spells a different word or phrase backwards. ("Semordnilap" is itself "palindromes" spelled backwards.) Write a semordnilap recogniser that accepts a file name (pointing to a list of words) from the user and finds and prints all pairs of words that are semordnilaps to the screen. For example, if "stressed" and "desserts" is part of the word list, the the output should include the pair "stressed desserts". Note, by the way, that each pair by itself forms a palindrome!"""
+"""According to Wikipedia, a semordnilap is a word or phrase that spells a different word or phrase backwards. ("Semordnilap" is itself 
+"palindromes" spelled backwards.) Write a semordnilap recogniser that accepts a file name (pointing to a list of words) from the user and 
+finds and prints all pairs of words that are semordnilaps to the screen. For example, if "stressed" and "desserts" is part of the word 
+list, the the output should include the pair "stressed desserts". Note, by the way, that each pair by itself forms a palindrome!"""
 
 
 filename=input("Enter the name of the file ")
