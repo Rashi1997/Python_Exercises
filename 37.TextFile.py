17795f28b6535796e2c0094ed9293acba663bef5
Rashi1997 <rdhar1997@gmail.com>
2018-11-02 14:04:36 +0530

    Add files via upload

--- 37.TextFile.py
+++ 37.TextFile.py
@@ -1,6 +1,6 @@
-"""Write a program that given a text file will create a new text file in which all the lines from the original file are numbered from 1 to 
-n (where n is the number of lines in the file)."""
-f=open("C:/Users/rasdhar/Desktop/Python Training/37/text_file.txt")
+"""Write a program that given a text file will create a new text file in which all the lines from the original file are numbered from 1 to n (where n is the number of lines in the file)."""
+loc="C:/Users/rasdhar/Desktop/Python Training/37/text_file.txt"
+f=open(loc)
 lines=f.read().split("\n")
 f.close()
 f_output=open("C:/Users/rasdhar/Desktop/Python Training/37/list_text_file.txt",'w')
