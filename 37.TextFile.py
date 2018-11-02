060c346e0c7723a08d08761ba315033fdf2b9c89
Rashi1997 <rdhar1997@gmail.com>
2018-11-02 12:36:44 +0530

    Update 37.TextFile.py

--- 37.TextFile.py
+++ 37.TextFile.py
@@ -1,4 +1,5 @@
-"""Write a program that given a text file will create a new text file in which all the lines from the original file are numbered from 1 to n (where n is the number of lines in the file)."""
+"""Write a program that given a text file will create a new text file in which all the lines from the original file are numbered from 1 to 
+n (where n is the number of lines in the file)."""
 f=open("C:/Users/rasdhar/Desktop/Python Training/37/text_file.txt")
 lines=f.read().split("\n")
 f.close()
